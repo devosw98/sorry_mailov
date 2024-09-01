2f94f7993fc6d9b8b89ce1da1028628a24e50432
devosw98 <dev.osw98cd@gmail.com>
2024-09-01 10:00:35 -0600

    mod para crecimiento de botones

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const currentWidth = yesBtn.offsetWidth;
         const currentHeight = yesBtn.offsetHeight;
 
-        yesBtn.style.width = currentWidth * 1.1 + 'px';
-        yesBtn.style.height = currentHeight * 1.1 + 'px';
+        yesBtn.style.width = currentWidth * 1.3 + 'px';
+        yesBtn.style.height = currentHeight * 1.3 + 'px';
 
         const currentFontSize = window.getComputedStyle(yesSpn).fontSize;
-        yesSpn.style.fontSize = parseFloat(currentFontSize) * 1.1 + 'px';
+        yesSpn.style.fontSize = parseFloat(currentFontSize) * 1.3 + 'px';
 
         const currentBorderRadius = window.getComputedStyle(yesBtn).borderRadius;
-        yesBtn.style.borderRadius = parseFloat(currentBorderRadius) * 1.1 + 'px';
+        yesBtn.style.borderRadius = parseFloat(currentBorderRadius) * 1.3 + 'px';
     }),
 
     yesBtn.addEventListener('click', () => {
